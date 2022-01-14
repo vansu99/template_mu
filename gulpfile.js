fb574823db9e20378d649c5b84fbd70a5e66b8a4
su.tran <vansutran99@gmail.com>
2022-01-14 20:33:13 +0700

    update image for sections

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require("gulp");
 const pug = require("gulp-pug");
-const sass = require("gulp-sass");
+const sass = require("gulp-sass")(require('sass'));
 const imagemin = require("gulp-imagemin");
 const uglify = require("gulp-uglify");
 const babel = require("gulp-babel");
